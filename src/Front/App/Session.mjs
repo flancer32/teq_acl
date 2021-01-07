c23417a93d96c604170e27ee35c3160a70dbce68
Alex Gusev <alex@flancer64.com>
2021-01-07 11:19:41 +0200

    LEAN-115 Add sign out route to desk realm

--- src/Front/App/Session.mjs
+++ src/Front/App/Session.mjs
@@ -6,17 +6,39 @@ export default class Fl32_Teq_Acl_Front_App_Session {
 
     constructor(spec) {
         /** @type {TeqFw_Core_App_Obj_Factory} */
-        const objFactory = spec.TeqFw_Core_App_Obj_Factory$;
+        const objFactory = spec['TeqFw_Core_App_Obj_Factory$']; // singleton instance
+        /** @type {Fl32_Teq_User_Defaults} */
+        const DEF_USER = spec['Fl32_Teq_User_Defaults$'];   // singleton instance
         /** @type {Fl32_Teq_User_Front_App_Session} */
-        const base = spec.Fl32_Teq_User_Front_App_Session$$;    // new instance
+        const base = spec['Fl32_Teq_User_Front_App_Session$$'];    // new instance
 
         // POPULATE CURRENT INSTANCE WITH BASE CLASSES METHODS (COMPOSITION INSTEAD OF INHERITANCE)
         objFactory.assignObjectMethods(this, base);
 
+        /**
+         * Return 'true' if user in session has given permission.
+         * @param {String} perm
+         * @return {Boolean}
+         */
         this.hasPermission = function (perm) {
             /** @type {Fl32_Teq_Acl_Shared_Service_Data_UserAcl} */
             const user = this.getUser();
             return user && user.permissions && Object.values(user.permissions).includes(perm);
+        };
+
+        /**
+         * Redirect to sign in route if user in session has no given permission.
+         * @param {Object} router Vue Router
+         * @param {String} perm
+         * @return {Promise<void>}
+         */
+        this.redirectOnFail = async function (router, perm) {
+            if (!this.hasPermission(perm)) {
+                const routeCurrent = router.currentRoute.value.path;
+                this.setRouteToRedirect(routeCurrent);
+                const routeSignIn = this.getRouteToSignIn() ?? DEF_USER.ROUTE_SIGN_IN;
+                await router.push(routeSignIn);
+            }
         };
     }
 
